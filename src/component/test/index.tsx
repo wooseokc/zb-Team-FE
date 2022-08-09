50888094697791bb9a5cdca5758744ce860b8b95
Wooseok <wschun95@naver.com>
2022-08-09 18:42:53 +0900

    0809 팀프로젝트 FE 첫 셋업 푸쉬
    TS
    StyleComponent
    React
    Axios
    RouterDom
    세팅

---
+++ src/component/test/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Test } from "./style";
+
+export default function TestBox () {
+
+  return (
+    <Test>
+      테스트용으로 만든 상자입니다
+    </Test>
+  )
+}
---
+++ src/component/test/style.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+export const Test = styled.div`
+  width : 100px;
+  height : 100px;
+  background : blue;
+`
