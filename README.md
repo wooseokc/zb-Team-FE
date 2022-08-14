7f2d9565d3e1d628bfec0a8357c03d6aad43e47d
Wooseok <wschun95@naver.com>
2022-08-14 22:06:58 +0900

    게임 기능 구현하고 배포

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 팀 백엔드 깃헙 : https://github.com/devjy39/minesweeper
+
+배포 url : https://minesweeper-2283e.web.app/
--- pages/style.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-
-export const Test = styled.a`
-  width : 100px;
-  height : 100px;
-  background : blue;
-`
