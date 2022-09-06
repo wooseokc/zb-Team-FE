b2cc5ddbf824862c551b7d9853f819160e5907be
이기문 <kimoon212@gmail.com>
2022-09-06 22:48:39 +0900

    이전 채팅 데이터 불러오기 기능 추가

--- components/border/Chating.tsx
+++ components/border/Chating.tsx
@@ -1,123 +1,174 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { ReactElement, useEffect, useState, useRef } from 'react'
+import React, { ReactElement, useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
 import { ws } from './chat/Chathook';
-// import axios from 'axios';
-
+import axios from 'axios';
 
 //https://minesweeper.hanjoon.dev/minesweeper/stomp/chat
 //subscribe('/topic/lobby'
-// /pub/lobby 
+// /pub/lobby
 
 function Chating() {
   const [text, setText] = useState<string>('');
   const [nickname, setNickname] = useState<string>('');
-  const [message, setMessage] = useState<{ name: string, message: string }[]>([]);
+  const [message, setMessage] = useState<{ name: string; message: string }[]>(
+    []
+  );
   const scrollRef = useRef();
-  
-  
-  
-  
+  const [lastIndex, setLastIndex] = useState<number>(0);
   let gameName: string | null = '';
   if (typeof window !== 'undefined') {
-    gameName = sessionStorage.getItem('gamerName')
+    gameName = sessionStorage.getItem('gamerName');
   }
   // const url = 'https://minesweeper.hanjoon.dev/minesweeper/stomp/chat'
   // let sock = new SockJS(url);
   // let ws = Stomp.over(sock);
-  
+
   const inputNick = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (nickname) {
       e.target.disabled = !e.target.disabled;
     }
-  }
+  };
 
   ws.onConnect = () => {
     ws.subscribe('/topic/lobby', (data) => {
       const res = JSON.parse(data.body);
       // console.log(res.name, 'nick'+nickname);
-        setMessage(prev => [...prev, {name: res.name, message: res.message}])  
-      
-    })
-  }
-  
+      setMessage((prev) => [...prev, { name: res.name, message: res.message }]);
+    });
+  };
+
   useEffect(() => {
-    // const chatdata = async () => {
-    //   const res = await axios.get('https://minesweeper.hanjoon.dev/minesweeper/chat-lobby/')
-    //   console.log(res);
-    // }
-    // chatdata();
+    const chatdata = async () => {
+      const res = await axios.get(
+        'https://minesweeper.hanjoon.dev/minesweeper/chat-lobby/'
+      );
+      setLastIndex(res.data.lastIndex);
+
+      if (res) {
+        res.data.chatList.map(
+          (item: { name: string; message: string }): boolean => {
+            setMessage((prev) => [
+              ...prev,
+              { name: item.name, message: item.message },
+            ]);
+            return true;
+          }
+        );
+      }
+    };
+    chatdata();
+
     if (gameName) {
       setNickname(gameName);
     }
     ws.activate();
     return () => {
       ws.deactivate();
-    }
+    };
   }, []);
 
   useEffect(() => {
-    const current : any = scrollRef.current;
+    const current: any = scrollRef.current;
     current.scrollTop = current.scrollHeight;
-  },[message])
+  }, [message]);
 
-  const send = (data: { name: string, message: string }) => {
+  const send = (data: { name: string; message: string }) => {
     if (ws.connected) {
-      
       ws.publish({
         destination: '/pub/lobby',
         body: JSON.stringify({ name: data.name, message: data.message }),
       });
     }
-  
-};
-  
+  };
 
-  const submit = async(e: React.FormEvent<HTMLFormElement>) => {
+  const oldchatdata = async () => {
+    const res = await axios.get(
+      `https://minesweeper.hanjoon.dev/minesweeper/chat-lobby/${lastIndex}`
+    );
+    console.log(res);
+    if (res) {
+      res.data.chatList.map(
+        (item: { name: string; message: string }): boolean => {
+          setMessage((prev) => [
+            { name: item.name, message: item.message },
+            ...prev,
+          ]);
+          return true;
+        }
+      );
+    }
+    if (lastIndex >= 0) {
+      setLastIndex((prev) => (prev -= 31));
+    }
+  };
+
+  const onScroll = (e) => {
+    console.log(e.target.scrollTop);
+    console.log(lastIndex);
+    if (e.target.scrollTop < 1) {
+      oldchatdata();
+    }
+  };
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = {
       name: nickname,
-      message: text
-    }
+      message: text,
+    };
     // setMessage([...message, data])
     send(data);
     setText('');
-    
-  }
+  };
 
   return (
     <>
-      <ChatingBox ref={scrollRef}>
+      <ChatingBox ref={scrollRef} onScroll={onScroll}>
         {message.map<ReactElement>((item, idx) => (
-          
-            <li key={idx}>
-              <NicknameSpan >{item.name} :</NicknameSpan>{item.message}
-            </li>
-          
+          <li key={idx}>
+            <NicknameSpan>{item.name} :</NicknameSpan>
+            {item.message}
+          </li>
         ))}
       </ChatingBox>
       <Inputform onSubmit={submit}>
-        {gameName
-          ? <InputText type='text' id='nickname' disabled value={nickname}  />
-          : <InputText type='text' id='nickname' maxLength={10} onChange={e => setNickname(e.target.value)} onBlur={inputNick} value={nickname} autoFocus required />
-        }
-        
-        <InputText type='text' id='message' onChange={(e) => setText(e.target.value)} value={text} />
-        <TextSubmit type='submit' value='작성하기'/>
+        {gameName ? (
+          <InputText type='text' id='nickname' disabled value={nickname} />
+        ) : (
+          <InputText
+            type='text'
+            id='nickname'
+            maxLength={10}
+            onChange={(e) => setNickname(e.target.value)}
+            onBlur={inputNick}
+            value={nickname}
+            autoFocus
+            required
+          />
+        )}
+
+        <InputText
+          type='text'
+          id='message'
+          onChange={(e) => setText(e.target.value)}
+          value={text}
+        />
+        <TextSubmit type='submit' value='작성하기' />
       </Inputform>
     </>
-  )
+  );
 }
 
-export default Chating
+export default Chating;
 
-const ChatingBox = styled.ul<{ref : any}>`
+const ChatingBox = styled.ul<{ ref: any }>`
   width: 500px;
   height: 500px;
 
   border: 2px solid #e6e6e6;
   border-radius: 15px;
-  
+
   margin: 0 auto;
   margin-top: 50px;
   padding: 0;
@@ -130,11 +181,11 @@ const ChatingBox = styled.ul<{ref : any}>`
   &::-webkit-scrollbar {
     width: 15px;
     background-color: #e6e6e6;
-    
+
     border-radius: 10px;
   }
-  &::-webkit-scrollbar-thumb{
-    background-color:#464646;
+  &::-webkit-scrollbar-thumb {
+    background-color: #464646;
     border-radius: 10px;
   }
 
@@ -158,8 +209,8 @@ const NicknameSpan = styled.span`
   display: inline-block;
 `;
 const InputText = styled.input`
-  width: ${props => props.id === 'nickname' ? '100' : '300'}px;
-  
+  width: ${(props) => (props.id === 'nickname' ? '100' : '300')}px;
+
   box-sizing: border-box;
 `;
 const TextSubmit = styled.input`
